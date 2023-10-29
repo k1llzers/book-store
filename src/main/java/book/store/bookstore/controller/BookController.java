4c91ea45ba36690ea72fb3cd48bb2fae130e94a8
Oleksandr Semytskyi <ssemitskiy@gmail.com>
2023-10-29 12:35:17 +0200

    create BookController

---
+++ src/main/java/book/store/bookstore/controller/BookController.java
@@ -0,0 +1,35 @@
+package book.store.bookstore.controller;
+
+import book.store.bookstore.dto.BookDto;
+import book.store.bookstore.dto.CreateBookRequestDto;
+import book.store.bookstore.service.BookService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/books")
+public class BookController {
+    private final BookService bookService;
+
+    @GetMapping
+    public List<BookDto> getAll() {
+        return bookService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public BookDto getBookById(@PathVariable Long id) {
+        return bookService.findById(id);
+    }
+
+    @PostMapping
+    public BookDto createBook(@RequestBody CreateBookRequestDto createBookRequestDto) {
+        return bookService.save(createBookRequestDto);
+    }
+}
