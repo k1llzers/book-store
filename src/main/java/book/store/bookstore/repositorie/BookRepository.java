f63fe0d8c27b28a8b08d9ca6ff7f16b4946c5e81
Oleksandr Semytskyi <ssemitskiy@gmail.com>
2023-10-29 12:33:12 +0200

    add findById to BookRepository and BookRepositoryImpl

--- src/main/java/book/store/bookstore/repositorie/BookRepository.java
+++ src/main/java/book/store/bookstore/repositorie/BookRepository.java
@@ -2,9 +2,12 @@ package book.store.bookstore.repositorie;
 
 import book.store.bookstore.model.Book;
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository {
     Book save(Book book);
 
     List<Book> findAll();
+
+    Optional<Book> findById(Long id);
 }
