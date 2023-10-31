6bac55502933cb25f2bfd9bd2c2cb30054ef0e61
Oleksandr Semytskyi <ssemitskiy@gmail.com>
2023-11-01 00:33:53 +0200

    add spec provider manager

---
+++ src/main/java/book/store/bookstore/repository/book/BookSpecificationProviderManager.java
@@ -0,0 +1,23 @@
+package book.store.bookstore.repository.book;
+
+import book.store.bookstore.model.Book;
+import book.store.bookstore.repository.SpecificationProvider;
+import book.store.bookstore.repository.SpecificationProviderManager;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class BookSpecificationProviderManager implements SpecificationProviderManager<Book> {
+    private final List<SpecificationProvider<Book>> bookSpecificationProviders;
+
+    @Override
+    public SpecificationProvider<Book> getSpecificationProvider(String key) {
+        return bookSpecificationProviders.stream()
+                .filter(p -> p.getKey().equals(key))
+                .findFirst()
+                .orElseThrow(() ->
+                        new RuntimeException("Can`t find specification provider for key: " + key));
+    }
+}
